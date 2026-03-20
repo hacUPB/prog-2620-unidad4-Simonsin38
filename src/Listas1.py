8777d14fdd4f3b1a90167c717dd2a729cf080319
Simón_Osorio <simonsin4001@gmail.com>
2026-03-20 15:05:05 -0500

    Codigo de unidad 4

--- src/Listas1.py
+++ src/Listas1.py
@@ -1,2 +1,8 @@
-hola = [[1, 2, 3], [5, 6, 7], [8, 9 , 3]]
-print(hola)
+# hola = [[1, 2, 3], [5, 6, 7], [8, 9 , 3]]
+# print(hola)
+lista = []
+for i in range(3):
+   lista.append(int(input(f"Ingrese el valor #{i+1} de la lista: ")))
+print(lista)
+
+list = []
