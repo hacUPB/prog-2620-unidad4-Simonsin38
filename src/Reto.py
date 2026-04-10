2a357662f23909a3bc6edc487e2ab77ea7d9f526
Simón_Osorio <simonsin4001@gmail.com>
2026-04-10 15:23:51 -0500

    Se avanza con el reto

--- src/Reto.py
+++ src/Reto.py
@@ -6,14 +6,25 @@ for i in range(3):
     matricula = input(f"Ingrese la matricula del {aeronave}: ")
     modelo = input(f"Ingrese el modelo del {aeronave}: ")
     horas = float(input(f"Ingrese las horas de vuelo del {aeronave}: "))
-    motorih = float(input("Ingrese las horas de uso del motor izquierdo"))
-    motoril = float(input("Ingrese as horas limite antes del mantenimiento del motor izquierdo"))
-    motordh = float(input("Ingrese las horas de uso del motor izquierdo"))
-    motordl = float(input("Ingrese as horas limite antes del mantenimiento del motor izquierdo"))
+    motorih = float(input("Ingrese las horas de uso del motor izquierdo: "))
+    motoril = float(input("Ingrese las horas limite antes del mantenimiento del motor izquierdo: "))
+    motordh = float(input("Ingrese las horas de uso del motor derecho: "))
+    motordl = float(input("Ingrese las horas limite antes del mantenimiento del motor derecho: "))
+    trenh = float(input("Ingrese las horas de uso del tren de aterrizaje: "))
+    trenl = float(input("Ingrese las horas limite antes del mantenimiento del tren de aterrizaje: "))
+    alabesh = float(input("Ingrese las horas de uso de los alabes: "))
+    alabesl = float(input("Ingrese las horas limite antes del mantenimiento de los alabes: "))
     registro_aeronaves[aeronave] = {"Matricula" : matricula , "Modelo" : modelo , "Horas de la aeronave" : horas , 
                                     "Componentes" : {"Motor izquierdo" : {"Horas de uso" : motorih , "Horas Limite" : motoril} , 
-                                                     "Motor derecho" : {"Horas de uso" : motordh , "Horas Limite" : motordl}}}
+                                                     "Motor derecho" : {"Horas de uso" : motordh , "Horas Limite" : motordl}, 
+                                                     "Tren de Aterrizaje" : {"Horas de uso" : trenh , "Horas Limite" : trenl},
+                                                     "Alabes" : {"Horas de uso" : alabesh , "Horas Limite" : alabesl}}}
+for aeronave, datos in registro_aeronaves.items():
+    for componente, info in datos["Componentes"].items():
+        horas_uso = info["Horas de uso"]
+        horas_limite = info["Horas Limite"]
 
-    
-print(registro_aeronaves)
-
+        nombre_aeronave = aeronave
+        nombre_componente = componente
+        if horas_uso >= horas_limite:
+            print(f"El componente -{nombre_componente}- de el {nombre_aeronave} requiere mantenimiento")
